2815a89b870376b889c9bbd2cc5c51244d26c42b
Dipirona537 <dipirona28@gmail.com>
2024-10-27 19:43:47 -0400

    Adicionado Maven ao projeto

--- Trabalho - Gerencia de Configuracao - AppFacil/src/Main/main.java
+++ Trabalho - Gerencia de Configuracao - AppFacil/src/Main/main.java
@@ -1,17 +1,23 @@
 package Main;
 
 
+import BancoDeDados.Outros.CriarDB;
 import Telas.Garcom.TelaAdicionarPedidos;
 import Telas.Garcom.TelaPedidos;
 import Telas.Gestor.TelaAdicionarAoCardapio;
 import Telas.Sistema.TelaLogin;
 import Telas.Sistema.TelaZero;
 
+import javax.swing.*;
+
 public class main {
 
     public static void main(String[] args) throws Exception {
-        
-        boolean status = false;
+        if (!CriarDB.bancoExiste()) //Checa se banco ja existe, caso nao existir cria o arquivo do banco
+        {
+            CriarDB.criarArquivo();
+            JOptionPane.showMessageDialog(null, "Banco Criado!\nArquivo: "+CriarDB.localbanco);
+        }
 
         new TelaAdicionarAoCardapio().setVisible(false);
         new TelaLogin().setVisible(false);
